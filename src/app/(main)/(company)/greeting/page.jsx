ed95d5142417b6ee373fbc4ffd33c06c853a2e50
jungho <kiik52@naver.com>
2025-05-23 18:43:48 +0900

    - 회사소개 인사말 완료

--- src/app/(main)/(company)/greeting/page.jsx
+++ src/app/(main)/(company)/greeting/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Section01 from "./components/Section01";
+import Section02 from "./components/Section02";
 
 export default function page() {
   return (
     <div className="w-full min-h-screen overflow-hidden text-base">
       <Section01 />
+      <Section02 />
     </div>
   );
 }
